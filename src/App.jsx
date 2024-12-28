75e1526f814b9c018f4071133ccba6ec52c1574e
thevowels <aungphyohtet1@gmail.com>
2024-12-28 16:58:47 +0700

    Created necessary tables @supabase Added Navigation like thing to navabar.

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,35 @@ function App() {
     <div className="container">
         {!session ?<div className={"col-md-8 col-lg-5 mx-auto px-5"}>
             <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
-        </div>: <div>Logged In</div>}
+        </div>: <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <button className="nav-link w-100">Home</button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link w-100">People</button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link w-100">Pay</button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="nav-link w-100">Get</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </div>}
         {session && console.log(session)}
 
     </div>
