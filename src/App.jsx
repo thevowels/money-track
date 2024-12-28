ccaf46ff46fc5bc4c96c0fbea8a21b91e9b09e39
thevowels <aungphyohtet1@gmail.com>
2024-12-28 16:25:06 +0700

    Adding Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/style.scss'
+import {supabase} from "./utils/supabaseClient.js";
+import {Auth} from "@supabase/auth-ui-react";
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [session, setSession] = useState(null);
+
+    useEffect(() => {
+        supabase.auth.getSession()
+            .then(({data:{session}})=>{
+                setSession(session);
+            });
+        supabase.auth.onAuthStateChange((_event,session)=>{
+            setSession(session);
+        })
+    },[])
 
   return (
     <div className="container">
-        <button className={"btn btn-primary"}>Bootstrap</button>
+        {!session ?<div className={"col-md-8 col-lg-5 mx-auto px-5"}>
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
+        </div>: <div>Logged In</div>}
+        {session && console.log(session)}
+
     </div>
   )
 }
