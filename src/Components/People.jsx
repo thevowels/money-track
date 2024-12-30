dbb95e01cad2ccc1565d3df0906260bbc37ff419
thevowels <aungphyohtet1@gmail.com>
2024-12-31 04:48:45 +0700

    Used bootstrap css based modal and remove the modal on success using the js. Make it by selecting the elements and its not the right way todo but I want to stick with it for a while

--- src/Components/People.jsx
+++ src/Components/People.jsx
@@ -9,6 +9,7 @@ export default function People({session}) {
     const [people, setPeople] = useState(null);
     const [display, setDisplay] = useState("primary");
     const [toEdit, setToEdit] = useState(null);
+    const [toRemove, setToRemove] = useState(null);
     const goBack = () => setDisplay("primary");
     useEffect(() => {
         fetchPeople();
@@ -28,6 +29,14 @@ export default function People({session}) {
         setDisplay("edit");
 
     }
+    async function removePeople(){
+        console.log(toRemove);
+        var myModalEl = document.querySelector('#exampleModal');
+        myModalEl.display="none"
+        myModalEl.classList.remove('show');
+        document.getElementsByClassName("modal-backdrop")[0].remove();
+
+    }
 
     if(error) return <div>
         You've faced an error. Please contact the developer
@@ -37,6 +46,32 @@ export default function People({session}) {
     if(people && display === "primary") {
         return(
             <div>
+
+                {/*Modal*/}
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                     aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                Are you sure you want to remove {toRemove ? toRemove.name:''}
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close
+                                </button>
+                                <button type="button" className="btn btn-primary" onClick={removePeople}>I'm Sure</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                {/*Modal*/}
+
                 <table className="table table-striped table-hover user-table table-bordered">
                     <caption>People list</caption>
                     <thead className="bg-info text-light text-center">
@@ -57,34 +92,43 @@ export default function People({session}) {
                             <td>{p.phone}</td>
                             <td className={"d-none d-sm-table-cell"}>{p.nrc}</td>
                             <td className={"d-none d-sm-table-cell"}>{p.address || '-'}</td>
-                            <td className={"text-center"}> <button className={"btn btn-outline-primary px-3"} onClick={()=>editPeople(p) }>Edit</button> </td>
+                            <td className={"text-center"}>
+                                <button className={"btn btn-outline-primary px-3 me-1 btn-sm"}
+                                        onClick={() => editPeople(p)}>Edit
+                                </button>
+                                <button className={"btn btn-outline-primary px-3 btn-sm m"} data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                        onClick={() => setToRemove(p)}>Remove
+                                </button>
+                            </td>
                         </tr>) :
-                        <tr className="text-center h-25" >
-                            <td colSpan={5} style={{lineHeight:'100px'}}>
+                        <tr className="text-center h-25">
+                            <td colSpan={5} style={{lineHeight: '100px'}}>
                                 You have not add any people yet.
                                 <br/>
-                                <button className="btn btn-outline-primary" onClick={() => setDisplay('add')}>Add People</button>
+                                <button className="btn btn-outline-primary" onClick={() => setDisplay('add')}>Add
+                                    People
+                                </button>
                             </td>
                         </tr>
                     }
                     </tbody>
                 </table>
                 {people.length > 0 ? <div className={"text-end"}>
-                    <button className="btn btn-outline-primary" onClick={()=>setDisplay('add')}>Add People</button>
-                </div>: null}
+                    <button className="btn btn-outline-primary" onClick={() => setDisplay('add')}>Add People</button>
+                </div> : null}
             </div>
         )
 
     }
-    if(display ==="add"){
-        return(
+    if (display === "add") {
+        return (
             <>
                 <AddPeople session={session} goBack={goBack}/>
             </>
         )
     }
-    if(display == "edit") {
-        return(
+    if (display == "edit") {
+        return (
             <>
                 <EditPeople toEdit={toEdit} goBack={goBack}/>
             </>
