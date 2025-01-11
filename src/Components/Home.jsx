77cfe85066b7d5c06c9cc74e48c4d32fe8a44360
thevowels <aungphyohtet1@gmail.com>
2025-01-12 02:51:03 +0700

    Show overall loan and return on homePage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,9 +7,25 @@ import InfoCard from "./People/InfoCard.jsx";
 import PersonCard from "./People/PersonCard.jsx";
 
 export default function Home({session}) {
-
+    const [overall, setOverall] = useState({loan:0,return:0});
     useEffect(()=>{
-
+        let loan_return= {loan:0,return:0}
+        supabase.from("loan_return")
+            .select("*")
+            .then(({data, error})=>{
+                if(error){
+                    console.log(error)
+                }else if(data){
+                    data.forEach((record)=>{
+                        if(record.loan){
+                            loan_return['loan']+=record.amount;
+                        }else{
+                            loan_return['return']+=record.amount;
+                        }
+                    })
+                    setOverall(loan_return);
+                }
+            })
     },[])
 
     return(
@@ -24,30 +40,18 @@ export default function Home({session}) {
                     <div className={"homeInfo col"}>
                         <Card style={{width: '18rem'}} id={"outgoing"}>
                             <Card.Body>
-                                <Card.Title>Total Out Last 30 days</Card.Title>
+                                <Card.Title className={"currency"}>Total Loan</Card.Title>
                                 <div className="row currency out-mmk ">
                                     <div className="col-6 text-end p-0">
-                                        {currency(5000000, {separator: ',', symbol: "", precision: 0}).format()}
+                                        {currency(overall.loan, {separator: ',', symbol: "", precision: 0}).format()}
                                     </div>
                                     <div className="col-3 text-start ps-2">
                                         MMK
                                     </div>
-                                    <div className="offset-1 col-2 text-center p-0">
-                                        <AiFillUpCircle color={"green"}/>
-                                        <AiFillDownCircle color={"red"}/>
-                                    </div>
-                                </div>
-                                <div className="row currency out-thb ">
-                                    <div className="col-6 text-end p-0">
-                                        {currency(3000, {separator: ',', symbol: "", precision: 0}).format()}
-                                    </div>
-                                    <div className="col-3 text-start ps-2">
-                                        THB
-                                    </div>
-                                    <div className="offset-1 col-2 text-center p-0">
-                                        <AiFillUpCircle color={"green"}/>
-                                        <AiFillDownCircle color={"red"}/>
-                                    </div>
+                                    {/*<div className="offset-1 col-2 text-center p-0">*/}
+                                    {/*    <AiFillUpCircle color={"green"}/>*/}
+                                    {/*    <AiFillDownCircle color={"red"}/>*/}
+                                    {/*</div>*/}
                                 </div>
                                 {/*<Card.Subtitle className="mb-2 text-blue">Card Subtitle</Card.Subtitle>*/}
                             </Card.Body>
@@ -57,30 +61,18 @@ export default function Home({session}) {
                     <div className={"homeInfo col"}>
                         <Card style={{width: '18rem'}} id={"incoming"}>
                             <Card.Body>
-                                <Card.Title>Total Out Last 30 days</Card.Title>
+                                <Card.Title className={"currency"}>Total Return</Card.Title>
                                 <div className="row currency in-mmk ">
                                     <div className="col-6 text-end p-0">
-                                        {currency(5000000, {separator: ',', symbol: "", precision: 0}).format()}
+                                        {currency(overall.return, {separator: ',', symbol: "", precision: 0}).format()}
                                     </div>
                                     <div className="col-3 text-start ps-2">
                                         MMK
                                     </div>
-                                    <div className="offset-1 col-2 text-center p-0">
-                                        <AiFillUpCircle color={"green"}/>
-                                        <AiFillDownCircle color={"red"}/>
-                                    </div>
-                                </div>
-                                <div className="row currency in-thb ">
-                                    <div className="col-6 text-end p-0">
-                                        {currency(3000, {separator: ',', symbol: "", precision: 0}).format()}
-                                    </div>
-                                    <div className="col-3 text-start ps-2">
-                                        THB
-                                    </div>
-                                    <div className="offset-1 col-2 text-center p-0">
-                                        <AiFillUpCircle color={"green"}/>
-                                        <AiFillDownCircle color={"red"}/>
-                                    </div>
+                                    {/*<div className="offset-1 col-2 text-center p-0">*/}
+                                    {/*    <AiFillUpCircle color={"green"}/>*/}
+                                    {/*    <AiFillDownCircle color={"red"}/>*/}
+                                    {/*</div>*/}
                                 </div>
                                 {/*<Card.Subtitle className="mb-2 text-blue">Card Subtitle</Card.Subtitle>*/}
                             </Card.Body>
